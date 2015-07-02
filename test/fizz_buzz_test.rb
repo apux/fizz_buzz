da1af8d19b2eb0b6bc55433c96acf126678de915
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-07-02 11:31:35 -0500

    Refactor the test.

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -2,51 +2,28 @@ require 'fizz_buzz'
 require 'minitest/autorun'
 
 class FizzBuzzTest < Minitest::Test
-  def test_value_is_1_when_receives_1
-    assert_equal "1", FizzBuzz.new.value(1)
+  def test_value_is_fizz_when_number_is_multiple_of_3
+    [3, 6, 9, 12, 18, 21].each do |number|
+      assert_equal "Fizz", FizzBuzz.new.value(number)
+    end
   end
 
-  def test_value_is_2_when_receives_2
-    assert_equal "2", FizzBuzz.new.value(2)
+  def test_value_is_buzz_when_number_is_multiple_of_5
+    [5, 10, 20, 25, 35, 40].each do |number|
+      assert_equal "Buzz", FizzBuzz.new.value(number)
+    end
   end
 
-  def test_value_is_fizz_when_receives_3
-    assert_equal "Fizz", FizzBuzz.new.value(3)
+  def test_value_is_buzz_when_number_is_multiple_of_3_and_5
+    [15, 30, 45, 60, 75, 90].each do |number|
+      assert_equal "FizzBuzz", FizzBuzz.new.value(number)
+    end
   end
 
-  def test_value_is_4_when_receives_4
-    assert_equal "4", FizzBuzz.new.value(4)
-  end
 
-  def test_value_is_buzz_when_receives_5
-    assert_equal "Buzz", FizzBuzz.new.value(5)
-  end
-
-  def test_value_is_fizz_when_receives_6
-    assert_equal "Fizz", FizzBuzz.new.value(6)
-  end
-
-  def test_value_is_fizz_when_receives_9
-    assert_equal "Fizz", FizzBuzz.new.value(9)
-  end
-
-  def test_value_is_buzz_when_receives_10
-    assert_equal "Buzz", FizzBuzz.new.value(10)
-  end
-
-  def test_value_is_fizzbuzz_when_receives_15
-    assert_equal "FizzBuzz", FizzBuzz.new.value(15)
-  end
-
-  def test_value_is_buzz_when_receives_20
-    assert_equal "Buzz", FizzBuzz.new.value(20)
-  end
-
-  def test_value_is_fizzbuzz_when_receives_30
-    assert_equal "FizzBuzz", FizzBuzz.new.value(30)
-  end
-
-  def test_value_is_fizzbuzz_when_receives_45
-    assert_equal "FizzBuzz", FizzBuzz.new.value(45)
+  def test_value_is_the_number_when_number_is_not_multiple_of_3_or_5
+    [1, 2, 4, 7, 11, 14, 16, 19, 43].each do |number|
+      assert_equal number.to_s, FizzBuzz.new.value(number)
+    end
   end
 end
